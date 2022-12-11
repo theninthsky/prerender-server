17ffda0f35bde4a566643c6f11ceea1a31083027
Almog Gabay <theninesky@gmail.com>
2022-12-11 15:24:30 +0200

    Revert "Debug render.com pwd"
    This reverts commit fef342ad71b24ebae6c5f6c36c3ba2905315e97e.

--- build.sh
+++ build.sh
@@ -2,10 +2,16 @@
 # exit on error
 set -o errexit
 
-echo "...Downloading Chrome"
-mkdir -p ./chrome
-cd ./chrome
-wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -x ./google-chrome-stable_current_amd64.deb ./
-rm ./google-chrome-stable_current_amd64.deb
-cd $HOME/project/src # Make sure we return to where we were
+STORAGE_DIR=/opt/render/project/.render
+
+if [[ ! -d $STORAGE_DIR/chrome ]]; then
+  echo "...Downloading Chrome"
+  mkdir -p $STORAGE_DIR/chrome
+  cd $STORAGE_DIR/chrome
+  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  rm ./google-chrome-stable_current_amd64.deb
+  cd $HOME/project/src # Make sure we return to where we were
+else
+  echo "...Using Chrome from cache"
+fi
--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import prerender from 'prerender'
 import memoryCache from 'prerender-memory-cache'
-import { exec } from 'child_process'
-
-exec('pwd')
 
 const server = prerender({
-  chromeLocation: './chrome/opt/google/chrome/test',
+  chromeLocation: '/opt/render/project/.render/chrome/opt/google/chrome/',
   chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars'],
   waitAfterLastRequest: 0.2 * 1000,
   pageDoneCheckInterval: 0.5 * 1000,
