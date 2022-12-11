e217df6a38e5b2a5fb837f67cc6a08e88fc88871
Almog Gabay <theninesky@gmail.com>
2022-12-11 10:59:33 +0200

    Set manual chrome location for render.com

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import prerender from 'prerender'
 import memoryCache from 'prerender-memory-cache'
 
 const server = prerender({
+  chromeLocation: '/opt/render/project/.render/chrome/opt/google/chrome/',
   waitAfterLastRequest: 0.2 * 1000,
   pageDoneCheckInterval: 0.5 * 1000,
   pageLoadTimeout: 5 * 1000
