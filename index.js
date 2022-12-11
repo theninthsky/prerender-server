55694d46f57f575c69f54d5629a34567024747ee
Almog Gabay <theninesky@gmail.com>
2022-12-11 11:19:29 +0200

    Add 'no-sandbox' option to prerender

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import memoryCache from 'prerender-memory-cache'
 
 const server = prerender({
   chromeLocation: '/opt/render/project/.render/chrome/opt/google/chrome/',
+  chromeFlags: ['--no-sandbox', '--headless'],
   waitAfterLastRequest: 0.2 * 1000,
   pageDoneCheckInterval: 0.5 * 1000,
   pageLoadTimeout: 5 * 1000
