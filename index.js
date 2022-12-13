a946bdf2fc412b559c6cc889aef08205ec00fa85
Almog Gabay <theninesky@gmail.com>
2022-12-13 22:27:07 +0200

    Attempt to remove chrome flags for cyclic.sh

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import memoryCache from 'prerender-memory-cache'
 
 const server = prerender({
   chromeLocation: await Chromium.executablePath,
-  chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222'],
   waitAfterLastRequest: 0.2 * 1000,
   pageDoneCheckInterval: 0.5 * 1000,
   pageLoadTimeout: 5 * 1000
