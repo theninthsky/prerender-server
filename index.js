aa93acc823b513d1bd61c3e11a4a39debc0e192c
Almog Gabay <theninesky@gmail.com>
2022-12-13 22:03:27 +0200

    Create startServer function

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ import Chromium from 'chrome-aws-lambda'
 import prerender from 'prerender'
 import memoryCache from 'prerender-memory-cache'
 
-const server = prerender({
-  chromeLocation: await Chromium.executablePath,
-  chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars'],
-  waitAfterLastRequest: 0.2 * 1000,
-  pageDoneCheckInterval: 0.5 * 1000,
-  pageLoadTimeout: 5 * 1000
-})
+const startServer = async () => {
+  const chromeLocation = await Chromium.executablePath
 
-server.use(prerender.removeScriptTags())
-server.use(memoryCache)
+  const server = prerender({
+    chromeLocation,
+    chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars'],
+    waitAfterLastRequest: 0.2 * 1000,
+    pageDoneCheckInterval: 0.5 * 1000,
+    pageLoadTimeout: 5 * 1000
+  })
 
-server.start()
+  server.use(prerender.removeScriptTags())
+  server.use(memoryCache)
+
+  server.start()
+}
+
+startServer()
