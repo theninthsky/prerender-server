ffddcd2cf958d4f9afe80b10089e8bb1374f097b
Almog Gabay <theninesky@gmail.com>
2022-12-13 22:22:20 +0200

    Reintroduce necessary chrome flags

--- index.js
+++ index.js
@@ -2,21 +2,15 @@ import Chromium from 'chrome-aws-lambda'
 import prerender from 'prerender'
 import memoryCache from 'prerender-memory-cache'
 
-const startServer = async () => {
-  const chromeLocation = await Chromium.executablePath
+const server = prerender({
+  chromeLocation: await Chromium.executablePath,
+  chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222'],
+  waitAfterLastRequest: 0.2 * 1000,
+  pageDoneCheckInterval: 0.5 * 1000,
+  pageLoadTimeout: 5 * 1000
+})
 
-  const server = prerender({
-    chromeLocation,
-    chromeFlags: ['--no-sandbox', '--headless'],
-    waitAfterLastRequest: 0.2 * 1000,
-    pageDoneCheckInterval: 0.5 * 1000,
-    pageLoadTimeout: 5 * 1000
-  })
+server.use(prerender.removeScriptTags())
+server.use(memoryCache)
 
-  server.use(prerender.removeScriptTags())
-  server.use(memoryCache)
-
-  server.start()
-}
-
-startServer()
+server.start()
