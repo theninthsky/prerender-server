28a4a5841b85242fb6769e8841ae6fb7d58f6956
Almog Gabay <theninesky@gmail.com>
2022-12-13 22:11:48 +0200

    Remove unnecessary chrome flags

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const startServer = async () => {
 
   const server = prerender({
     chromeLocation,
-    chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars'],
+    chromeFlags: ['--no-sandbox', '--headless'],
     waitAfterLastRequest: 0.2 * 1000,
     pageDoneCheckInterval: 0.5 * 1000,
     pageLoadTimeout: 5 * 1000
