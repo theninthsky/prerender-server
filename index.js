ed7ddf2d787c776841fd5702b6dd99deb455baf5
Almog Gabay <theninesky@gmail.com>
2022-12-13 21:20:23 +0200

    Use chrome-aws-lambda

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+import Chromium from 'chrome-aws-lambda'
 import prerender from 'prerender'
 import memoryCache from 'prerender-memory-cache'
 
 const server = prerender({
-  chromeLocation: '/opt/render/project/.render/chrome/opt/google/chrome/',
+  chromeLocation: await Chromium.executablePath,
   chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars'],
   waitAfterLastRequest: 0.2 * 1000,
   pageDoneCheckInterval: 0.5 * 1000,
