46daf5892e02a8610029a2a439482d66ea41718f
Almog Gabay <theninesky@gmail.com>
2022-12-14 10:11:44 +0200

    Adjust prerender config to match render.com's free tier CPU speed #3

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import prerenderMemoryCache from 'prerender-memory-cache'
 
 const server = prerender({
   chromeLocation: executablePath(),
-  waitAfterLastRequest: 0.2 * 1000,
-  pageDoneCheckInterval: 0.5 * 1000,
+  waitAfterLastRequest: 2 * 1000,
+  pageDoneCheckInterval: 2 * 1000,
   pageLoadTimeout: 10 * 1000
 })
 
